c0b1c37d6a31049c97aa5546bb8ae0dccfcde5c4
nikitabhaskar <nikitab2204@gmail.com>
2025-03-18 15:07:50 -0400

    added example

---
+++ example.py
@@ -0,0 +1,12 @@
+from fortunecookiepackage import get_random_fortune, get_lucky_numbers, get_daily_fortune, get_custom_fortune, get_themed_fortune
+
+print(get_random_fortune()) 
+#Output: Sell your ideas—they have exceptional merit.
+print(get_lucky_numbers(6))
+#Output: Lucky Numbers: [83, 40, 3, 45, 66, 17]
+print(get_daily_fortune("Monday"))
+#Output: Today's Fortune: Goodness is the only investment that never fails.
+print(get_custom_fortune("Alice"))
+#Output: Alice, A journey of a thousand miles begins with a single step.
+print(get_themed_fortune("Love"))
+#Output: The one you love is closer than you think.
